8f312c08a142a1d72ae62a4298418316b8166f6e
KomalBassan91 <75784954+KomalBassan91@users.noreply.github.com>
2022-08-07 00:24:07 +0200

    remove extensive logs

--- src/main/java/com/mobiquity/test/client/RestAssuredClient.java
+++ src/main/java/com/mobiquity/test/client/RestAssuredClient.java
@@ -4,20 +4,20 @@ import io.restassured.*;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-public class RestAssuredClient extends RestAssured{
+public class RestAssuredClient extends RestAssured {
 
     public String baseUrl;
 
-    public RestAssuredClient(String baseUrl){
-        this.baseUrl=baseUrl;
+    public RestAssuredClient(String baseUrl) {
+        this.baseUrl = baseUrl;
     }
 
 
-    public Response httpGet(String url){
+
+    public Response httpGet(String url) {
         RequestSpecification httpGet = RestAssured.given().log().uri();
-        //RequestSpecification httpGet = RestAssured.given().log().all(true);
         httpGet.baseUri(baseUrl);
-        httpGet.header("Content-Type","application/json");
+        httpGet.header("Content-Type", "application/json");
         return httpGet.get(url);
     }
 
